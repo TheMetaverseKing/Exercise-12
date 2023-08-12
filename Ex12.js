5356d762f7050517256963f4d8fc19418c7b88e4
TheMetaverseKing <139680450+TheMetaverseKing@users.noreply.github.com>
2023-08-13 01:09:38 +0500

    Add files via upload

---
+++ Ex12.js
@@ -0,0 +1,8 @@
+"use strict";
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+var bestfriend = ["Abu Bakar", "Umar", "Usman", "Ali"];
+let message = " is one of the Khulafa-e-Rashedeen";
+console.log(bestfriend[0] + message);
+console.log(bestfriend[1] + message);
+console.log(bestfriend[2] + message);
+console.log(bestfriend[3] + message);
---
+++ Ex12.ts
@@ -0,0 +1,9 @@
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+
+var bestfriend:string [] = ["Abu Bakar", "Umar", "Usman", "Ali"]
+let message:string = " is one of the Khulafa-e-Rashedeen"
+console.log(bestfriend[0] + message)
+console.log(bestfriend[1] + message)
+console.log(bestfriend[2] + message)
+console.log(bestfriend[3] + message)
